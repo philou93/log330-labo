81cfb6eee6414b8e0f2c4b388ab262004b8122da
Philippe <you@example.com>
2018-05-28 12:25:45 -0400

    Correction d'erreur de division

--- tests/test_ecart-type.py
+++ tests/test_ecart-type.py
@@ -2,26 +2,22 @@ import pytest
 from MyMathModule import MyMathModule as mmm
 
 
-numbers = []
-def before_test():
-    numbers = [186, 699, 132, 272, 291, 331, 199, 1890, 788, 1601]
-
 # Les fonctions de test doit debuter par test_
 # On verifie si une erreur est lancé si la liste des nombres est vide et on divise par 0
 def test_CT4():
-    before_test()
+    numbers = [186, 699, 132, 272, 291, 331, 199, 1890, 788, 1601]
     mean = mmm.mean(numbers)
     var = mmm.variance(numbers,mean)
     assert mmm.std_derivation(var) > 625
 
 def test_CT5():
-    before_test()
+    numbers = [186, 699, 132, 272, 291, 331, 199, 1890, 788, 1601]
     mean = mmm.mean(numbers)
     var = mmm.variance(numbers,mean)
     assert mmm.std_derivation(var) < 626
 
 def test_CT6():
-    before_test()
+    numbers = [186, 699, 132, 272, 291, 331, 199, 1890, 788, 1601]
     mean = mmm.mean(numbers)
     var = mmm.variance(numbers,mean)
     assert mmm.std_derivation(var) != 625.1
--- tests/test_variance.py
+++ tests/test_variance.py
@@ -2,23 +2,19 @@ import pytest
 from MyMathModule import MyMathModule as mmm
 
 
-numbers = []
-def before_test():
-    numbers = [186, 699, 132, 272, 291, 331, 199, 1890, 788, 1601]
-
 # Les fonctions de test doit debuter par test_
 # On verifie si une erreur est lancé si la liste des nombres est vide et on divise par 0
 def test_CT1():
-    before_test()
+    numbers = [186, 699, 132, 272, 291, 331, 199, 1890, 788, 1601]
     mean = mmm.mean(numbers)
     assert mmm.variance(numbers,mean) > 391417
 
 def test_CT2():
-    before_test()
+    numbers = [186, 699, 132, 272, 291, 331, 199, 1890, 788, 1601]
     mean = mmm.mean(numbers)
     assert mmm.variance(numbers,mean) < 391418
 
 def test_CT3():
-    before_test()
+    numbers = [186, 699, 132, 272, 291, 331, 199, 1890, 788, 1601]
     mean = mmm.mean(numbers)
     assert mmm.variance(numbers,mean) != 5656
