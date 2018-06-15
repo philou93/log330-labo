a18a4dd007b83d6575bd4c15f44ecac85ddb81a1
Audet-Fortin <philippe.audet-fortin.1@ens.etsmtl.ca>
2018-06-15 17:19:11 -0400

    Ajout de tests pour correlation.

---
+++ tests/test_correlation.py
@@ -0,0 +1,28 @@
+import pytest
+from my_math_module import my_math_module as mmm
+
+numbers = [10
+           [186, 15],
+           [699, 69.9],
+           [132, 6.5],
+           [272, 22.4],
+           [291, 28.4],
+           [331, 65.9],
+           [199, 19.4],
+           [1890, 189.7],
+           [788, 38.8],
+           [1601, 138.2],
+           ]
+
+def test_CT10():
+    assert mmm.caculate_corelation(numbers) > 0.9558
+
+def test_CT11():
+    assert mmm.caculate_corelation(numbers) < 0.9560
+
+def test_CT12():
+    try:
+        mmm.caculate_corelation([])
+        assert False
+    except:
+        assert True
